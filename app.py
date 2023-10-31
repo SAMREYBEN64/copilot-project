6a607952d10153f8d29bd992ff3029b567fc9929
Samuel Reyes Benavides <79462926+SAMREYBEN64@users.noreply.github.com>
2023-10-31 04:15:25 +0000

    Game Finished Successfully

--- app.py
+++ app.py
@@ -3,9 +3,39 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
-app = Flask(__name__)
+import random
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+
+options = ["rock", "paper", "scissors"]
+score = {"player": 0, "computer": 0}
+
+while True:
+    computer_choice = random.choice(options)
+    player_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+
+    if player_choice not in options:
+        print("Invalid option. Please try again.")
+        continue
+
+    print(f"Computer chose {computer_choice}")
+
+    if player_choice == computer_choice:
+        print("It's a tie!")
+    elif (player_choice == "rock" and computer_choice == "scissors") or \
+            (player_choice == "paper" and computer_choice == "rock") or \
+            (player_choice == "scissors" and computer_choice == "paper"):
+        print("You win!")
+        score["player"] += 1
+    else:
+        print("You lose!")
+        score["computer"] += 1
+
+    print(f"Score: Player {score['player']}, Computer {score['computer']}")
+
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != "yes":
+        break
+
+
+
+
